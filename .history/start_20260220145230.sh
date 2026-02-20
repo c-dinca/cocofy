6e72f23b5e7eedbe653c0986bf8cf5af76e4ce4c
Cristian Andrei <93765979+CristianAndrei233@users.noreply.github.com>
2026-02-20 17:09:34 +0100

    cleanup: remove .history

--- .history/start_20260220145230.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export MUSIC_DIR=/tmp/cocofy-music
-export CACHE_DIR=/tmp/cocofy-cache
-mkdir -p $MUSIC_DIR $CACHE_DIR
-python3 -m uvicorn app:app --reload --port 8888
--- .history/start_20260220151318.sh
+++
@@ -1,7 +0,0 @@
-echo '#!/bin/bash
-export MUSIC_DIR=/tmp/cocofy-music
-export CACHE_DIR=/tmp/cocofy-cache
-mkdir -p $MUSIC_DIR $CACHE_DIR
-/opt/homebrew/bin/python3 -m pip install -r requirements.txt -q
-/opt/homebrew/bin/python3 -m pip install yt-dlp -q
-/opt/homebrew/bin/python3 -m uvicorn app:app --reload --port 8888' > start.sh
